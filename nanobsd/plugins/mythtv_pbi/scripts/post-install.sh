4cc891920d6ef7a5889b90291ed87cb5bee29925
ProtoSD <sdproto@gmail.com>
2012-09-24 10:31:49 -0700

    Changed jdown to myth in rc script

--- nanobsd/plugins/mythtv_pbi/scripts/post-install.sh
+++ nanobsd/plugins/mythtv_pbi/scripts/post-install.sh
@@ -3,7 +3,7 @@
 MYTHTV_HOME=/usr/pbi/mythtv-`uname -m`
 
 # Add 2 checkboxes to GUI to select which app to start (mythtv-setup, or backend)
-# Add mountpoint(s)
+# Add mountpoint(s) _Recordings
 
 mv ${MYTHTV_HOME}/libGL.so ${MYTHTV_HOME}/lib/
 mv ${MYTHTV_HOME}/libGL.so.1 ${MYTHTV_HOME}/lib/
@@ -33,15 +33,15 @@ mv ${MYTHTV_HOME}/sbin_mythtv ${MYTHTV_HOME}/sbin/
 #rm ${MYTHTV_HOME}/fonts.tar
 
 # Copy template RC script over existing script
-#cp -a ${MYTHTV_HOME}/rc_mythtvd ${MYTHTV_HOME}/etc/rc.d/mythtvd
-#cp -a ${MYTHTV_HOME}/rc_mythtvd /usr/local/etc/rc.d/mythtvd
-#chmod 755 /usr/local/etc/rc.d/mythtvd
+cp -a ${MYTHTV_HOME}/rc_mythtvd ${MYTHTV_HOME}/etc/rc.d/mythtvd
+cp -a ${MYTHTV_HOME}/rc_mythtvd /usr/local/etc/rc.d/mythtvd
+chmod 755 /usr/local/etc/rc.d/mythtvd
 
 mkdir -p ${MYTHTV_HOME}/etc/home/mythtv/.fluxbox
 
-#pw groupadd jdown
-#pw useradd jdown -g jdown -G wheel -s /bin/sh -d ${MYTHTV_HOME}/etc/home/mythtv -w none
-#chown -R jdown:jdown ${MYTHTV_HOME}/etc/home/mythtv
+#pw groupadd mythtv
+#pw useradd mythtv -g mythtv -G wheel -s /bin/sh -d ${MYTHTV_HOME}/etc/home/mythtv -w none
+#chown -R mythtv:mythtv ${MYTHTV_HOME}/etc/home/mythtv
 
 mkdir -p /var/run/MythTV /var/log/MythTV
 touch /var/run/MythTV/MythTV.pid /var/log/MythTV/MythTV.log
