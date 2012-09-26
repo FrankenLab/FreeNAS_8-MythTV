63280afff051d19579b121f8c531dbd3d521682d
ProtoSD <sdproto@gmail.com>
2012-09-25 17:47:26 -0700

    Tweak start scripts, mysqld calls wrapper to start Setup or Backend, linked hostname.pid /var/run/mysql.pid

--- nanobsd/plugins/mythtv_pbi/scripts/post-install.sh
+++ nanobsd/plugins/mythtv_pbi/scripts/post-install.sh
@@ -3,16 +3,11 @@
 MYTHTV_HOME=/usr/pbi/mythtv-`uname -m`
 MYTH_USER=mythtv
 
-# Add 2 checkboxes to GUI to select which app to start (mythtv-setup, or backend)
-# Add mountpoint(s) _Recordings
+${MYTHTV_HOME}/bin/python ${MYTHTV_HOME}/mythtvUI/manage.py syncdb --migrate --noinput
 
 # IF `uname -m` .eq. amd64
-#mv ${MYTHTV_HOME}/lib_x64/libGL.so ${MYTHTV_HOME}/lib/
-#mv ${MYTHTV_HOME}/lib_x64/libGL.so.1 ${MYTHTV_HOME}/lib/
-#mv ${MYTHTV_HOME}/lib_x64/libQtDBus.so ${MYTHTV_HOME}/lib/
-#mv ${MYTHTV_HOME}/lib_x64/libQtDBus.so.4 ${MYTHTV_HOME}/lib/
-#mv ${MYTHTV_HOME}/lib_x64/libQtDBus.so.4.8 ${MYTHTV_HOME}/lib/
-#mv ${MYTHTV_HOME}/lib_x64/libQtDBus.so.4.8.2 ${MYTHTV_HOME}/lib/
+mv ${MYTHTV_HOME}/lib_x64/libGL.so ${MYTHTV_HOME}/lib/
+mv ${MYTHTV_HOME}/lib_x64/libGL.so.1 ${MYTHTV_HOME}/lib/
 
 ldconfig -m ${MYTHTV_HOME}/lib
 ldconfig -m ${MYTHTV_HOME}/lib/mysql
@@ -20,16 +15,25 @@ ldconfig -m ${MYTHTV_HOME}/lib/mysql/plugin
 ldconfig -m ${MYTHTV_HOME}/lib/qt4/plugins
 
 mkdir -p ${MYTHTV_HOME}/_MythDatabase/mysql
+
 /usr/pbi/mythtv-`uname -m`/bin/mysql_install_db --basedir=/usr/pbi/mythtv-`uname -m` --datadir=/usr/pbi/mythtv-`uname -m`/_MythDatabase --force
-#${MYTHTV_HOME}/bin/mysql_update
+
 cp ${MYTHTV_HOME}/share/mythtv/database/mc.sql ${MYTHTV_HOME}/_MythDatabase/mc.sql
-/usr/pbi/mythtv-`uname -m`/bin/mysql -uroot -pmythtv< ${MYTHTV_HOME}/_MythDatabase/mc.sql
+/usr/pbi/mythtv-`uname -m`/bin/mysql -uroot < ${MYTHTV_HOME}/_MythDatabase/mc.sql
+
+# Database needs to be running for next command
+# Probably should run from sbin/mythtv
+#${MYTHTV_HOME}/bin/mysql_update
+
 
 mkdir -p ${MYTHTV_HOME}/_Recordings
 chmod 777 ${MYTHTV_HOME}/_Recordings
 
 mv ${MYTHTV_HOME}/sbin_mythtv ${MYTHTV_HOME}/sbin/mythtv
 chmod 755 ${MYTHTV_HOME}/sbin/mythtv
+
+mv ${MYTHTV_HOME}/mysql-server ${MYTHTV_HOME}/etc/rc.d/
+chmod 755 ${MYTHTV_HOME}/etc/rc.d/mysql-server
 
 ##########################
 # INSTALL FONTS FOR X11
@@ -43,14 +47,14 @@ chmod 755 ${MYTHTV_HOME}/sbin/mythtv
 #rm ${MYTHTV_HOME}/fonts.tar
 
 # Copy template RC script over existing script
-cp -a ${MYTHTV_HOME}/rc_mythtvd ${MYTHTV_HOME}/etc/rc.d/mythtvd
-cp -a ${MYTHTV_HOME}/rc_mythtvd /usr/local/etc/rc.d/mythtvd
-chmod 755 /usr/local/etc/rc.d/mythtvd
+#cp -a ${MYTHTV_HOME}/rc_mythtvd ${MYTHTV_HOME}/etc/rc.d/mythtvd
+#cp -a ${MYTHTV_HOME}/rc_mythtvd /usr/local/etc/rc.d/mythtvd
+#chmod 755 /usr/local/etc/rc.d/mythtvd
 
 mkdir -p ${MYTHTV_HOME}/etc/home/mythtv/.fluxbox
 
 pw groupadd ${MYTH_USER}
-pw useradd ${MYTH_USER} -g ${MYTH_USER} -G wheel -s /bin/sh -w none
+pw useradd ${MYTH_USER} -g ${MYTH_USER} -G wheel -s /bin/sh -w none -d ${MYTHTV_HOME}/etc/home/mythtv
 # Need to create home directory
 #pw useradd ${MYTH_USER} -g ${MYTH_USER} -G wheel -s /bin/sh -d ${MYTHTV_HOME}/etc/home/mythtv -w none
 #chown -R ${MYTH_USER}:${MYTH_USER} ${MYTHTV_HOME}/etc/home/mythtv
@@ -69,5 +73,3 @@ ldconfig -m ${MYTHTV_HOME}/lib
 
 echo 'mythtv_flags=""' >> ${MYTHTV_HOME}/etc/rc.conf
 echo 'mythtv_flags=""' >> /etc/rc.conf
-
-${MYTHTV_HOME}/bin/python ${MYTHTV_HOME}/mythtvUI/manage.py syncdb --migrate --noinput
