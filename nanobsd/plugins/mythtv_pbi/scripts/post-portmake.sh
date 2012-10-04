e81ef32d0b540ef05cfecf5be40e5ae13227b011
ProtoSD <sdproto@gmail.com>
2012-10-04 11:21:48 -0700

    Added post-portmake.sh to get rid of a bunch of docs and man pages that were using ~60MB

---
+++ nanobsd/plugins/mythtv_pbi/scripts/post-portmake.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+# PBI building script
+# This will run after your port build is complete
+##############################################################################
+
+mythtv_pbi_path=/usr/pbi/mythtv-$(uname -m)/
+
+find ${mythtv_pbi_path}/lib -iname "*.a" -delete
+rm -rf ${mythtv_pbi_path}/include
+rm -rf ${mythtv_pbi_path}/share/doc
+rm -rf ${mythtv_pbi_path}/share/emacs
+rm -rf ${mythtv_pbi_path}/share/examples
+rm -rf ${mythtv_pbi_path}/share/gettext
+rm -rf ${mythtv_pbi_path}/share/gtk-doc/html/gi
+rm -rf ${mythtv_pbi_path}/man
